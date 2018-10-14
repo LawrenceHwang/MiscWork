3e31ddcbb899db9363353b99261e405449e0739e
Lawrence Hwang <CoffeePoweredLion@gmail.com>
2018-10-14 14:30:48 -0700

    Adding the Get-ReverserdString function and test
    Learning the basic operation of data structure/ algorithm...

---
+++ Get-ReversedString.ps1
@@ -0,0 +1,26 @@
+function Get-ReversedString {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [ValidateNotNull()]
+    [string]$String
+  )
+
+  $CharArray = $String.ToCharArray()
+
+  #region PowerShell way
+  <#
+  $Result = @()
+
+  for ($i = $CharArray.count-1; $i -ge 0 ; $i--){
+    $Result = $Result + $CharArray[$i]
+  }
+  $Result -join ''
+  #>
+  #endregion PowerShell way
+
+  #region .Net way
+  [array]::Reverse($CharArray)
+  -join $CharArray
+  #endregion .Net way
+}
