fe0bdc6dfe911527e71d8a86df0dd95b352c2bb6
Lawrence Hwang <coffeepoweredlion@gmail.com>
2019-07-05 00:15:48 -0700

    Fix formatting

--- Save-EC2ConsoleScreenshot.ps1
+++ Save-EC2ConsoleScreenshot.ps1
@@ -2,7 +2,7 @@
 .Synopsis
    Saves the console screenshot of an Amazon EC2 instance. Requries an existing connection to AWS.
 .DESCRIPTION
-   This function retrieves the Amazon EC2 instance screenshot data, convert it back to bytes and saves it as jpeg. 
+   This function retrieves the Amazon EC2 instance screenshot data, convert it back to bytes and saves it as jpeg.
    By default, it won't overwrite the existing file but you can use the -overwrite switch to change the behaviour.
    The file path should end with .jpeg.
 .EXAMPLE
@@ -12,55 +12,53 @@
    VERBOSE: File already exists. Removing.
    VERBOSE: Image saved
 #>
-Function Save-EC2ConsoleScreenshot{
+Function Save-EC2ConsoleScreenshot {
 
     [cmdletbinding()]
     param(
-        [parameter(Mandatory=$true,
+        [parameter(Mandatory = $true,
             ValueFromPipeline = $true,
-            Position=0)]
+            Position = 0)]
         [ValidateNotNullOrEmpty()]
         [string]$instance,
-        
-        [parameter(Mandatory=$true,
-            Position=1)]
+
+        [parameter(Mandatory = $true,
+            Position = 1)]
         [ValidateNotNullOrEmpty()]
         [string]$filepath,
 
         [switch]$overwrite
     )
 
-    Begin{
-        if (!(Get-AWSCredentials)){
+    Begin {
+        if (!(Get-AWSCredentials)) {
             Write-Warning 'Not connected to AWS'
             break
         }
-       
+
     }
-    
-    Process{
-        try{
+
+    Process {
+        try {
             $byte = [convert]::FromBase64String((Get-EC2ConsoleScreenshot -InstanceId $instance -ErrorAction Stop).ImageData)
-        
-            if (test-path -Path $filepath){
-                if ($overwrite){
+
+            if (Test-Path -Path $filepath) {
+                if ($overwrite) {
                     Write-Verbose -Message 'File already exists. Removing.'
                     Remove-Item $filepath
-                }
-                else{
+                } else {
                     Write-Verbose -Message 'File already exists. Existing and no change was made. Consider using -overwrite.'
                     break
                 }
             }
-         
-            [io.file]::WriteAllBytes($filepath,$byte)
+
+            [io.file]::WriteAllBytes($filepath, $byte)
             Write-Verbose -Message 'Image saved'
-        }
-        catch{
+        } catch {
             Write-Warning 'Error gettting screenshot or saving file.'
         }
     }
-        
-    End{
+
+    End {
     }
 }
